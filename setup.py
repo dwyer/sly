655fdf5c5cef0203bdc09b4485a4628c47d6a29e
Casey Dwyer <caseydwyer@gmail.com>
2016-12-28 04:52:00 -0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#-*-coding:utf-8-*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+setup(
+    name='sly',
+    packages=['sly'],
+    version='0.1',
+    description='A lexer and parser library.',
+    author='Casey Dwyer',
+    author_email='caseydwyer@gmail.com',
+    url='https://github.com/dwyer/sly',
+    license='BSD',
+)
